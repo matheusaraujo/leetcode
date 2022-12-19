0f12819e81178fee2916a5ecb5cac7068c063f5a
Matheus Araujo <matheus.saraujo@gmail.com>
2022-12-19 09:44:40 +0000

    [LeetCode Sync] - Symmetric Tree - Runtime - 8 ms (12.53%), Memory - 16.2 MB (99.95%)

---
+++ 0101-symmetric-tree/solution.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    bool isMirror(TreeNode* left, TreeNode* right) {
+        if (left == NULL && right == NULL) return true;
+        if (left == NULL || right == NULL) return false;
+        if (left->val != right->val) return false;
+
+        return isMirror(left->left, right->right)
+            && isMirror(left->right, right->left);
+    }
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (root == NULL) return true;
+        return isMirror(root->left, root->right);
+    }
+};
