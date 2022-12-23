14c4bd6a7860d67cd41d9562b6deafbb6ad777f3
Matheus Araujo <matheus.saraujo@gmail.com>
2022-12-23 20:13:45 +0000

    [LeetCode Sync] - Number of Provinces - Runtime - 18 ms (80.03%), Memory - 13.8 MB (100.00%)

---
+++ 0547-number-of-provinces/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    int N;
+    void dfs(vector<vector<int>>& a, vector<int>& v, int i, int c) {
+        v[i] = c;
+        for(int k = 0; k < N; k++)
+            if (v[k] == 0 && a[i][k] == 1) dfs(a, v, k, c);
+    }
+
+public:
+    int findCircleNum(vector<vector<int>>& a) {
+        N = a.size();
+        int c = 0;
+        auto v = vector<int>(N);
+
+        for(int i = 0; i < N; i++) {
+            if (v[i] == 0) cout << i << endl, dfs(a, v, i, ++c);
+        }
+
+        return c;
+    }
+};
