7295ed950508e509a9d4b40da3d803d13b1a09ca
Matheus Araujo <matheus.saraujo@gmail.com>
2022-12-23 10:22:00 +0000

    [LeetCode Sync] - Largest Perimeter Triangle - Runtime - 43 ms (5.68%), Memory - 6.6 MB (63.64%)

---
+++ 1018-largest-perimeter-triangle/solution.go
@@ -0,0 +1,9 @@
+func largestPerimeter(nums []int) int {
+    sort.Ints(nums)
+    for i := len(nums)-1; i >= 2; i-- {
+        if nums[i-2] + nums[i-1] > nums[i] {
+            return nums[i-2] + nums[i-1] + nums[i]
+        }
+    }
+    return 0
+}
