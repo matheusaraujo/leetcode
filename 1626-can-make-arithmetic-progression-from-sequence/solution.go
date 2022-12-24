ba9b0c0a858439d3a5faa5ed89a30c4f8f227507
Matheus Araujo <matheus.saraujo@gmail.com>
2022-12-24 13:07:48 +0000

    [LeetCode Sync] - Can Make Arithmetic Progression From Sequence - Runtime - 0 ms (100.00%), Memory - 2.4 MB (100.00%)

---
+++ 1626-can-make-arithmetic-progression-from-sequence/solution.go
@@ -0,0 +1,8 @@
+func canMakeArithmeticProgression(arr []int) bool {
+    sort.Ints(arr)
+    var d = arr[1] - arr[0]
+    for i := 2; i < len(arr); i++ {
+        if arr[i] - arr[i-1] != d { return false }
+    }
+    return true
+}
