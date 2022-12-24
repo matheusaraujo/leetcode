55e0411ecb2ee1fbee0bb14991c57ec06b33e945
Matheus Araujo <matheus.saraujo@gmail.com>
2022-12-24 12:45:15 +0000

    [LeetCode Sync] - Employees With Missing Information - Runtime - 462 ms (93.57%), Memory - 0B (100.00%)

---
+++ 2110-employees-with-missing-information/solution.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select employee_id from Employees where employee_id not in (select employee_id from Salaries) union
+select employee_id from Salaries where employee_id not in (select employee_id from Employees)
+order by 1
