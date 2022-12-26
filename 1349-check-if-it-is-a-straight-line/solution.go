1971ee06eb6ee58c30c9e726a28253afd0470b34
Matheus Araujo <matheus.saraujo@gmail.com>
2022-12-26 14:32:58 +0000

    [LeetCode Sync] - Check If It Is a Straight Line - Runtime - 7 ms (28.92%), Memory - 3.5 MB (18.07%)

---
+++ 1349-check-if-it-is-a-straight-line/solution.go
@@ -0,0 +1,12 @@
+func isCollinear(x1 int, y1 int, x2 int, y2 int, x3 int, y3 int) bool {
+    return x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2) == 0
+}
+
+func checkStraightLine(c [][]int) bool {
+    for i := 2; i < len(c); i++ {
+        if !isCollinear(c[i-2][0], c[i-2][1], c[i-1][0], c[i-1][1], c[i][0], c[i][1]) { 
+            return false
+        }
+    }
+    return true
+}
