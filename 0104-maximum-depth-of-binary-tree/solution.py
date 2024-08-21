a2aef6c61a3a36310de716f996cce37884b3bece
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-21 16:24:39 +0000

    [LeetCode Sync] - Maximum Depth of Binary Tree - Runtime - 41 ms (49.25%), Memory - 17.7 MB (40.22%)

--- 0104-maximum-depth-of-binary-tree/solution.py
+++ 0104-maximum-depth-of-binary-tree/solution.py
@@ -6,6 +6,9 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root == None:
-            return 0
-        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        if not root: return 0
+        return 1 + max(
+            self.maxDepth(root.left),
+            self.maxDepth(root.right)
+        )
+
