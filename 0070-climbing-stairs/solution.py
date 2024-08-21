e9d97227ad8a273b160592d88df6f2ca4271d6b2
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-21 16:27:02 +0000

    [LeetCode Sync] - Climbing Stairs - Runtime - 34 ms (59.35%), Memory - 16.5 MB (14.69%)

---
+++ 0070-climbing-stairs/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 3: return n
+        n1, n2 = 2, 3
+
+        for i in range(4, n + 1):
+          tmp = n1 + n2
+          n1 = n2
+          n2 = tmp
+
+        return n2
+
