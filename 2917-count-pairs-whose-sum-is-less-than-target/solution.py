5dea36c998357218298447f2d82fa9fb6ca5de79
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-22 15:21:01 +0000

    [LeetCode Sync] - Count Pairs Whose Sum is Less than Target - Runtime - 54 ms (23.47%), Memory - 16.4 MB (71.21%)

---
+++ 2917-count-pairs-whose-sum-is-less-than-target/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        left, right, ans = 0, len(nums) - 1, 0
+
+        while left < right:
+            if nums[left] + nums[right] < target:
+                ans += right - left
+                left += 1
+            else:
+                right -= 1
+
+        return ans
+
+
