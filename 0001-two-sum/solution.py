933f353c4ec7a458187e3ead823da9b437b154eb
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-23 15:19:37 +0000

    [LeetCode Sync] - Two Sum - Runtime - 53 ms (87.42%), Memory - 17.7 MB (29.37%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -2,9 +2,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hist = {}
 
-        for idx, num in enumerate(nums):
-            diff = target - num
-            if diff in hist:
-                return [hist[diff], idx]
-            hist[num] = idx
+        for i, num in enumerate(nums):
+            if target - num in hist:
+                return [hist[target-num], i]
+            hist[num] = i
 
