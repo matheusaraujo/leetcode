a63a07f9144fbee6aa02e5ff169dcbee7da45028
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-23 16:07:38 +0000

    [LeetCode Sync] - Container With Most Water - Runtime - 517 ms (68.41%), Memory - 29.9 MB (6.01%)

---
+++ 0011-container-with-most-water/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right, ans = 0, len(height) - 1, 0
+
+        while left < right:
+            area = (right - left) * min(height[left], height[right])
+            ans = max(ans, area)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -=1
+
+        return ans
+
+
