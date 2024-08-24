d0023558077fdc83eb3434d9364798c64d9ded1e
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-24 20:34:25 +0000

    [LeetCode Sync] - House Robber - Runtime - 32 ms (79.45%), Memory - 16.5 MB (26.92%)

---
+++ 0198-house-robber/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            tmp = max(rob1 + n, rob2)
+            rob1 = rob2
+            rob2 = tmp
+
+        return rob2
+
