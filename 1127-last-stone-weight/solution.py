dc2c70a2102f01b6f656fd1884dda5fb5d581940
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-28 15:02:44 +0000

    [LeetCode Sync] - Last Stone Weight - Runtime - 43 ms (9.81%), Memory - 16.4 MB (97.48%)

---
+++ 1127-last-stone-weight/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            x = heapq.heappop(stones)
+            y = heapq.heappop(stones)
+            if y > x:
+                heapq.heappush(stones, x - y)
+
+        return -stones[0] if len(stones) else 0
+
+
