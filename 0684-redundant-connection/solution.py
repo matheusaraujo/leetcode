c3a494de65843a06c455f14f686dd352a1dd3acd
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-28 21:48:02 +0000

    [LeetCode Sync] - Redundant Connection - Runtime - 49 ms (87.65%), Memory - 16.9 MB (81.51%)

---
+++ 0684-redundant-connection/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        parent = [i for i in range(len(edges) + 1)]
+        rank = [1] * (len(edges) + 1)
+
+        def find(n: int) -> int:
+            p = parent[n]
+            while p != parent[p]:
+                parent[p] = parent[parent[p]]
+                p = parent[p]
+            return p
+
+        def union(n1: int, n2: int) -> bool:
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2: return False
+
+            if rank[p1] > rank[p2]:
+                parent[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                parent[p1] = p2
+                rank[p2] += rank[p1]
+
+            return True
+
+        for n1, n2 in edges:
+            if not union(n1, n2): return [n1, n2]
+
+
+
