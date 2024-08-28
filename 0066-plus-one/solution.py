e7c54cbdb3d065ff33dc4c8a467f645e4deb6553
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-28 17:57:15 +0000

    [LeetCode Sync] - Plus One - Runtime - 37 ms (51.52%), Memory - 16.5 MB (22.91%)

--- 0066-plus-one/solution.py
+++ 0066-plus-one/solution.py
@@ -1,18 +1,12 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        n = len(digits)
-    
-        # Traverse the list in reverse order
-        for i in range(n-1, -1, -1):
-            if digits[i] < 9:
-                # If the current digit is less than 9, increment it by 1 and return
-                digits[i] += 1
-                return digits
+        d = len(digits) - 1
+        while d >= 0:
+            if digits[d] == 9:
+                digits[d] = 0
+                d -= 1
             else:
-                # If the current digit is 9, it becomes 0 and carry over 1 to the next digit
-                digits[i] = 0
-        
-        # If all digits are 9, we need an extra digit at the beginning
+                digits[d] += 1
+                return digits
         return [1] + digits
-        
-        
+
