cd2b08b1ab30d6de1ffbaf30e944db18dac9962b
Matheus Araujo <matheus.saraujo@gmail.com>
2024-09-04 21:28:30 +0000

    [LeetCode Sync] - Least Number of Unique Integers after K Removals - Runtime - 313 ms (94.40%), Memory - 33.3 MB (70.11%)

---
+++ src/1604-least-number-of-unique-integers-after-k-removals/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        # count = {}
+
+        # for n in arr: count[n] = 1 + count.get(n, 0)
+        # arr = sorted([(val, key) for key, val in count.items()])
+
+        # result, i = len(arr), 0
+        
+        # while k > 0 and i < len(arr):
+        #     if arr[i][0] <= k: result -= 1
+        #     k -= arr[i][0]
+        #     i += 1
+        
+        # return result
+        counter = list(Counter(arr).values())
+        heapq.heapify(counter)
+
+        while k > 0:
+            k -= heapq.heappop(counter)
+        return len(counter) + (k < 0)
+
