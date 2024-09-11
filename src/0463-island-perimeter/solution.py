3a543ab6cb3a356f3b7b6b758004912274ee8c95
Matheus Araujo <matheus.saraujo@gmail.com>
2024-09-11 20:27:44 +0000

    [LeetCode Sync] - Island Perimeter - Runtime - 343 ms (93.70%), Memory - 16.7 MB (99.61%)

---
+++ src/0463-island-perimeter/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+       def islandPerimeter(self, grid: List[List[int]]) -> int:
+        perimeter = 0
+        rows, cols = len(grid), len(grid[0])
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    perimeter += 4
+                    # Check the upper neighbor
+                    if i > 0 and grid[i - 1][j] == 1:
+                        perimeter -= 2
+                    # Check the left neighbor
+                    if j > 0 and grid[i][j - 1] == 1:
+                        perimeter -= 2
+        
+        return perimeter
