ad28bb7e35d9a0e390ca25ca1864540ae5bc8a01
Matheus Araujo <matheus.saraujo@gmail.com>
2024-10-02 12:11:52 +0000

    [LeetCode Sync] - Counter II - Runtime - 56 ms (72.09%), Memory - 51.5 MB (74.71%)

---
+++ src/2789-counter-ii/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let currentValue = init;
+    
+    return {
+        increment: () => ++currentValue,
+        decrement: () => --currentValue,
+        reset: () => {
+            currentValue = init;
+            return currentValue;
+        }
+    };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
